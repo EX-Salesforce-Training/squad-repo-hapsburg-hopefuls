d5ae40e9c9133e6f15d42d64ca17379402eca090
Matthew Lewandowski <mblewandowski96@gmail.com>
2021-11-11 17:47:47 -0500

    added calculator

---
+++ AboutMePage/script.js
@@ -0,0 +1,63 @@
+const buttons = document.querySelector("#buttons");
+const result = document.querySelector("#result");
+let firstOperand;
+let finished = false;
+let finalOperator;
+
+buttons.addEventListener('click', (evt) => {
+    if (evt.target.id === "buttons") return;
+    switch (evt.target.innerHTML) {
+        case "+":
+            setOperator("+")
+            break;
+        case "-":
+            setOperator("-")
+            break;
+        case "/":
+            setOperator("/")
+            break;
+        case "*":
+            setOperator("*")
+            break;
+        case "=":
+            calculate(result.innerHTML);
+            break;
+        default:
+            if (finished) {
+                result.innerHTML = evt.target.innerHTML;
+                finished = false;
+            } else {
+                if (result.innerHTML === "0") result.innerHTML = evt.target.innerHTML;
+                else result.innerHTML += evt.target.innerHTML;
+            }
+
+            break;
+    }
+})
+
+function setOperator(operator) {
+    firstOperand = parseFloat(result.innerHTML);
+    result.innerHTML = "0";
+    finalOperator = operator;
+}
+
+function calculate(secondOperand) {
+    if (firstOperand === null) return;
+
+    switch (finalOperator) {
+        case "+":
+            result.innerHTML = firstOperand + parseFloat(secondOperand);
+            break;
+        case "-":
+            result.innerHTML = firstOperand - parseFloat(secondOperand);
+            break;
+        case "/":
+            result.innerHTML = firstOperand / parseFloat(secondOperand);
+            break;
+        case "*":
+            result.innerHTML = firstOperand * parseFloat(secondOperand);
+            break;
+    }
+    firstOperand = null;
+    finished = true;
+}
