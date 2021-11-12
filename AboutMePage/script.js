2b2e70a66beefece30ad2d9cfa0ddbaefd2d7e6e
Matthew Lewandowski <mblewandowski96@gmail.com>
2021-11-12 00:19:48 -0500

    added better user experience for calc

--- AboutMePage/script.js
+++ AboutMePage/script.js
@@ -2,7 +2,11 @@ const buttons = document.querySelector("#buttons");
 const result = document.querySelector("#result");
 let firstOperand;
 let finished = false;
+let isOperating = false;
+let maxSize = 18;
 let finalOperator;
+
+
 
 buttons.addEventListener('click', (evt) => {
     if (evt.target.id === "buttons") return;
@@ -24,24 +28,17 @@ buttons.addEventListener('click', (evt) => {
             calculate(result.innerHTML);
             break;
         case "clear":
-            result.innerHTML = "0";
-            firstOperand = null;
-            operator = null;
+            reset();
             break;
         default:
-            if (finished) {
-                result.innerHTML = evt.target.innerHTML;
-                finished = false;
-            } else {
-                if (result.innerHTML === "0") result.innerHTML = evt.target.innerHTML;
-                else result.innerHTML += evt.target.innerHTML;
-            }
+            handleNumbers(evt);
             break;
     }
 })
 
 function setOperator(operator) {
     firstOperand = parseFloat(result.innerHTML);
+    isOperating = true;
     finalOperator = operator;
 }
 
@@ -51,17 +48,46 @@ function calculate(secondOperand) {
     switch (finalOperator) {
         case "+":
             result.innerHTML = firstOperand + parseFloat(secondOperand);
+            if (parseFloat(result.innerHTML) > 100000000000) result.style.fontSize = "1.5rem";
             break;
         case "-":
             result.innerHTML = firstOperand - parseFloat(secondOperand);
+            if (parseFloat(result.innerHTML) > 100000000000) result.style.fontSize = "1.5rem";
             break;
         case "/":
-            result.innerHTML = firstOperand / parseFloat(secondOperand);
+            if (parseFloat(secondOperand) === 0) result.innerHTML = "hey stop it";
+            else result.innerHTML = firstOperand / parseFloat(secondOperand);
+            if (parseFloat(result.innerHTML) > 100000000000) result.style.fontSize = "1.5rem";
             break;
         case "*":
             result.innerHTML = firstOperand * parseFloat(secondOperand);
+            if (parseFloat(result.innerHTML) > 100000000000) result.style.fontSize = "1.5rem";
             break;
     }
+
     firstOperand = null;
     finished = true;
+}
+
+function handleNumbers(evt) {
+    if (finished) {
+        result.innerHTML = evt.target.innerHTML;
+        finished = false;
+    } else {
+        if (result.innerHTML.length >= maxSize && !isOperating) return;
+
+        if (result.innerHTML === "0" || isOperating) {
+            result.innerHTML = evt.target.innerHTML;
+            isOperating = false;
+        } else result.innerHTML += evt.target.innerHTML;
+    }
+
+}
+
+function reset() {
+    result.innerHTML = "0";
+    firstOperand = null;
+    finalOperator = null;
+    operator = null;
+    result.style.fontSize = "2rem";
 }
